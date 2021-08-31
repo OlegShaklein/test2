94912a0ab6ec085b33a9c046e9985462030c6a3a
OlegShaklein <piramidon2003@googlemail.com>
2021-08-31 19:46:10 +0300

    test commit 1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //char grade = 'A';
+    //string phrase = "Giraffe Academy";
+    //int age = 50;
+    //double gpa = 2.3;
+    //bool isMale = true;
+    //cout << "asdafas" << endl;
+    //string Name = "Jonh";
+    //int age = 70;
+    cout << << endl;
+    cout << "Hello" << endl;
+    //cout << "He liked the name "<< Name << endl;
+    //cout << "But did not like being "<< age << endl;
+
+    return 0;
+
+}
